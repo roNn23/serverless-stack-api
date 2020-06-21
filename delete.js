d82608e7c8afc29093ed6d7c043435741132bf31
Ronny Neefe <ronny.neefe@aoe.com>
2020-06-21 19:39:12 +0200

    added serverless API

---
+++ delete.js
@@ -0,0 +1,19 @@
+import handler from "./libs/handler";
+import dynamoDb from "./libs/dynamodb";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  await dynamoDb.delete(params);
+
+  return { status: true };
+});
---
+++ libs/handler.js
@@ -0,0 +1,18 @@
+export default function handler(lambda) {
+  return function (event, context) {
+    return Promise.resolve()
+      .then(() => lambda(event, context))
+      .then((responseBody) => [200, responseBody])
+      .catch((e) => {
+        return [500, { error: e.message }];
+      })
+      .then(([statusCode, body]) => ({
+        statusCode,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(body),
+      }));
+  };
+}
